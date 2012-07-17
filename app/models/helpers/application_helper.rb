727642470efb5190828680e39b7e6bc375818415
Felix Abele <felix.abele@gmail.com>
2012-07-17 17:16:32 +0200

    transformed CSS-Stylesheet to Sass

---
+++ app/models/helpers/application_helper.rb
@@ -0,0 +1,12 @@
+module ApplicationHelper
+  
+  
+  # ==========================================
+  #   Glycos-Icon-Helper
+  # ==========================================
+  def icon_for( label, icon, color='' )
+    if color == 'white' then icon << ' icon-white' end
+    "<i class='#{icon}'></i> #{label}</a>".html_safe
+  end
+  
+end
