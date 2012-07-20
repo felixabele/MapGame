b73446e5493eab67d8c146405bb4852bbeeafd7b
f.abele <f.abele@Felixa.traffic-dealer>
2012-07-20 15:59:04 +0200

    Added maps of France and Grate Britain to the Game

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# === Create Maps ===
+
+# === Create Games ===
+
+# === Import Cities ===
+# City.do_import('germany', 1)
+# City.do_import('spain',   2)
+# City.do_import('italy',   3)
+# City.do_import('france',  4)
+# City.do_import('england', 5)
