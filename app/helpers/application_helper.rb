1e4382431f7cbd43e390e944c380ccdc397bcb34
f.abele <f.abele@Felixa.traffic-dealer>
2012-07-31 15:53:39 +0200

    JSON was not 100% valid removed double closed a-tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
   # --- Icon
   def icon_for( label, icon, color='' )
     if color == 'white' then icon << ' icon-white' end
-    "<i class='#{icon}'></i> #{label}</a>".html_safe
+    "<i class='#{icon}'></i> #{label}".html_safe
   end
   
 end
--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -7,10 +7,10 @@ module MapsHelper
   def init_map( map )
     js = ""
     js << "$('#map_#{map.country}').geocloud({"
-    js << " width: #{map.width}, height: #{map.height},"
-    js << " map_src: '/assets/maps/#{map.map_file}',"
-    js << " geo_settings: {#{map.geo_settings}},"
-    js << " ref_point: {#{map.ref_point}}"
+    js << " \"width\": #{map.width}, height: #{map.height},"
+    js << " \"map_src\": \"/assets/maps/#{map.map_file}\","
+    js << " \"geo_settings\": {#{map.geo_settings}},"
+    js << " \"ref_point\": {#{map.ref_point}}"
     js << "});"
     js.html_safe
   end
@@ -21,7 +21,7 @@ module MapsHelper
   def cities_for_geocloud( cities, size=10 )
     ds = Array.new
     cities.each do |city|
-      ds << "{title: '#{city.name}', size: #{size}, coord: [#{city.lon},#{city.lat}], id: #{city.id}}"
+      ds << "{\"title\": \"#{city.name}\", \"size\": #{size}, \"coord\": [#{city.lon},#{city.lat}], \"id\": #{city.id}}"
     end
     "[#{ds.join(',')}]".html_safe
   end
